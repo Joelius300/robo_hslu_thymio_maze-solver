76fa4eb68e9eff310f64b47c588e130db374e897
Joel L <joelius300@protonmail.com>
2022-11-07 18:40:50 +0100

    [Untested] Add guide that just goes through instructions

--- joel-playground.py
+++ joel-playground.py
@@ -42,6 +42,25 @@ class RightHandGuide(Guide):
         return Direction.U_TURN
 
 
+class OrderedInstructionsGuide(Guide):
+    def __init__(self, instructions: list[Direction]):
+        self._instructions = instructions
+        self._instruction_index = 0
+
+    def on_detected_crossing(self, options: Direction) -> Direction:
+        if self._instruction_index >= len(self._instructions):
+            return Direction.STOP
+
+        dir = self._instructions[self._instruction_index]
+        if dir not in options:
+            raise ValueError(f"Could not submit instruction {self._instruction_index}: {dir} as it's not available on "
+                             "the current intersection.")
+
+        self._instruction_index += 1
+
+        return dir
+
+
 def _get_directions(left, right, front):
     dir = Direction.U_TURN
     if left:
