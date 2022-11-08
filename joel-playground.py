3092178a1ab84ffb52684972f78e36c02e989ede
Joel L <joelius300@protonmail.com>
2022-11-08 11:29:07 +0100

    Damn boi he maze

--- joel-playground.py
+++ joel-playground.py
@@ -1,8 +1,8 @@
 import time
-from typing import Optional
-
 from abc import ABC, abstractmethod
 from enum import Flag, auto
+from typing import Optional
+
 from thymio_python.thymiodirect import ThymioObserver, SingleSerialThymioRunner
 from thymio_python.thymiodirect.thymio_constants import PROXIMITY_FRONT_BACK, MOTOR_LEFT, MOTOR_RIGHT, BUTTON_CENTER
 
@@ -38,6 +38,8 @@ class RightHandGuide(Guide):
             return Direction.RIGHT
         elif Direction.STRAIGHT in options:
             return Direction.STRAIGHT
+        elif Direction.LEFT in options:
+            return Direction.LEFT
 
         return Direction.U_TURN
 
@@ -53,8 +55,9 @@ class OrderedInstructionsGuide(Guide):
 
         dir = self._instructions[self._instruction_index]
         if dir not in options:
-            raise ValueError(f"Could not submit instruction {self._instruction_index}: {dir} as it's not available on "
-                             "the current intersection.")
+            print(f"Could not submit instruction {self._instruction_index}: {dir} as it's not available on "
+                  "the current intersection.")
+            return Direction.STOP
 
         self._instruction_index += 1
 
@@ -102,7 +105,7 @@ class MazeWalker(ThymioObserver):
       Still, changes are that this doesn't even pose an issue but I'd rather have it documented.
     """
 
-    ninety_degree_time_ns = 2247700724  # comes from quite a bit of testing with Thymio 17
+    ninety_degree_time_ns = 2247700724 * .95  # comes from quite a bit of testing with Thymio 17
     turn_timings = {
         Direction.LEFT: {
             "left": -100,
@@ -126,8 +129,9 @@ class MazeWalker(ThymioObserver):
         self.guide = guide
         self.base_speed = 200
         self.diff_threshold = 75  # above what diff value should you steer to stay parallel
-        self.prox_threshold = 100  # below what value should you consider a proximity value to indicate an opening in the maze
-        self.front_prox_threshold = 3000  # front is more sensitive so if we want to be able to drive closer to the wall we need a higher threshold
+        self.opening_prox_threshold = 100  # below what value should you consider a proximity value to indicate an opening in the maze
+        self.on_intersection_prox_threshold = 2000  # above what proximity value should left and right indicate that you have reached the intersection
+        self.front_space_prox_threshold = 3200  # front is more sensitive so if we want to be able to drive closer to the wall we need a higher threshold
         self.turn_initiation_time = 0
         self.current_turn = Direction.STRAIGHT
         self.waiting_until_intersection_in_direction: Optional[Direction] = None
@@ -153,48 +157,66 @@ class MazeWalker(ThymioObserver):
 
         print("Left:", prox_front_left, "|", "Right:", prox_front_right, "|", "Front:", prox_front_center)
 
-        space_left = prox_front_left < self.prox_threshold
-        space_right = prox_front_right < self.prox_threshold
-        space_front = prox_front_center < self.front_prox_threshold
+        opening_left = prox_front_left < self.opening_prox_threshold
+        opening_right = prox_front_right < self.opening_prox_threshold
+        opening_front = prox_front_center < self.opening_prox_threshold
 
         diff = prox_front_left - prox_front_right  # positive if left wall is closer -> correction right
 
         # only do correction if in a corridor
-        if not space_left and not space_right and abs(diff) > self.diff_threshold:
+        if not opening_left and not opening_right and abs(diff) > self.diff_threshold and not self.waiting_until_intersection_in_direction:
             correction = round(diff / 50)
             self._set_motors(self.base_speed + correction, self.base_speed - correction)
         else:
             self._set_motors_straight()
 
         # get all possible directions we can currently see in -> potential turns
-        possible_dirs = _get_directions(space_left, space_right, space_front)
+        possible_dirs = _get_directions(opening_left, opening_right, opening_front)
 
-        if not self.waiting_until_intersection_in_direction:
-            print("No intersection in sight.")
+        is_on_intersection = False
+        # either waiting on dead end, or we see there will be a dead end soon in which case we don't want the side
+        # sensors as they hit the front wall too early so use the front center sensor
+        if self.waiting_until_intersection_in_direction == Direction.STRAIGHT or not opening_front:
+            is_on_intersection = prox_front_center > self.front_space_prox_threshold
+        # we're waiting on an intersection to the right, check if we see the perpendicular wall right
+        elif self.waiting_until_intersection_in_direction == Direction.RIGHT:
+            is_on_intersection = prox_front_right > self.on_intersection_prox_threshold
+        # we're waiting on an intersection to the left, check if we see the perpendicular wall left
+        elif self.waiting_until_intersection_in_direction == Direction.LEFT:
+            is_on_intersection = prox_front_left > self.on_intersection_prox_threshold
+
+        if is_on_intersection:
+            # We just reached the center of the intersection we were waiting for.
+            # Now we can ask the guide where we should go next and initiate the turn the guide instructs us to take.
+            # Of course, we need to give it the possible directions we queried when we first saw the intersection
+            # because now that direction is not deemed possible anymore because we can see the wall again.
+            self.waiting_until_intersection_in_direction = None
+            if Direction.STRAIGHT in self.last_possible_directions and not opening_front:
+                print("Didn't see front blockade when spotting the intersection; removing STRAIGHT from possibilities.")
+                self.last_possible_directions &= ~Direction.STRAIGHT
+            self.current_turn = self.guide.on_detected_crossing(self.last_possible_directions)
+            self.turn_initiation_time = time.time_ns()
+            print("On intersection; ready to take a turn.")
+            print(f"Chose {self.current_turn} from {{{self.last_possible_directions}}}")
+        # awaiting a dead end may be overridden if we encounter an actual intersection -> also updates the possible directions to take
+        elif not self.waiting_until_intersection_in_direction or self.waiting_until_intersection_in_direction == Direction.STRAIGHT:
             # we are not waiting until we are on an intersection, check if we should be
             if Direction.RIGHT in possible_dirs:
                 self.waiting_until_intersection_in_direction = Direction.RIGHT
             elif Direction.LEFT in possible_dirs:
                 self.waiting_until_intersection_in_direction = Direction.LEFT
-        elif self.waiting_until_intersection_in_direction not in possible_dirs:
-            # we are waiting until we are on an intersection and just reached the middle of that intersection because
-            # the opening that we were looking at earlier isn't visible anymore meaning we're currently located just
-            # before the next wall aka on the intersection. Now we can ask the guide where we should go next and
-            # initiate the turn the guide instructs us to take. Of course, we need to give it the possible directions
-            # we queried the last time because now that direction is not deemed possible anymore because we can see the
-            # wall again.
-            self.waiting_until_intersection_in_direction = None
-            self.current_turn = self.guide.on_detected_crossing(self.last_possible_directions)  # TODO since this takes the directions from the last tick, how does it behave when there is an intersection that has left and/or right open but not front? Does it see that it cannot go straight anymore or is the threshold too high currently?
-            self.turn_initiation_time = time.time_ns()
-            print("On intersection; ready to take a turn.")
-            print(f"Chose {self.current_turn} from {{{self.last_possible_directions}}}")
-        else:
-            # we are still waiting to get to the middle of the intersection but currently the opening to the side is
-            # still visible meaning we're not there yet, so just keep on moving.
-            assert (self.waiting_until_intersection_in_direction in possible_dirs)
-            print("Moving to the center of the intersection.")
+            elif Direction.STRAIGHT not in possible_dirs:  # handle dead ends the same way as intersections
+                self.waiting_until_intersection_in_direction = Direction.STRAIGHT
 
-        self.last_possible_directions = possible_dirs
+            # store options the thymio has for taking a turn once it's on the intersection.
+            # we could also query the guide here just so you know.
+            if self.waiting_until_intersection_in_direction:
+                print(f"Intersection in sight: {self.waiting_until_intersection_in_direction}")
+                print(f"Possible directions as of now: {possible_dirs}")
+                self.last_possible_directions = possible_dirs
+        else:
+            # we are still waiting to get to the middle of the intersection but not there yet, so just keep on moving.
+            print(f"Moving to the center of the intersection: {self.waiting_until_intersection_in_direction}")
 
     def do_turn(self):
         left, right, duration = self._get_timings(self.current_turn)
